46f6faddca786a425af104b4eaef2db6c81b371a
Ayush Shivam <166586488+ayushshivam48@users.noreply.github.com>
2024-09-02 09:22:21 +0530

    Add files via upload

---
+++ basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void india(){
+    cout<<"I'm Ayush Shivam"<<endl;
+}
+void info(){
+    india();
+    cout<<"Study in Amity Unvirsity Patna";
+
+}
+int main(){
+    info();
+}
